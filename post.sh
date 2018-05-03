9e2944465c7e98eb6d74c21573940e76fa29aaaa
muklislatif <muklislatif@gmail.com>
2018-05-03 21:44:03 +0700

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+# export TKPENV to define environtment
+# first param are index
+# ex: post.sh product
+
 URL="http://devel-solr.tkpd:9200/product/_bulk"
 
 if [ "$TKPENV" = 'staging' ]; then
-        URL=`http://10.255.13.51:9200`
+	URL=`http://10.255.13.51:9200`
 elif [ "$TKPENV" = 'production' ]; then
-        URL=`http://10.255.13.51:9200`
+	URL=`http://10.255.13.51:9200`
 fi
 
 output=""
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        output="$(curl -s -H "Content-Type: application/x-ndjson" -XPOST $URL --data-binary "@testshop.json")"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        output="$(curl -s -H "Content-Type: application/x-ndjson" -XPOST $URL --data-binary "@testshop.json")"
+if [[ "$1" != "" ]]; then
+        if [[ "$OSTYPE" == "linux-gnu" ]]; then
+                output="$(curl -s -H "Content-Type: application/x-ndjson" -XPOST $URL --data-binary "@$1.min.json")"
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+                output="$(curl -s -H "Content-Type: application/x-ndjson" -XPOST $URL --data-binary "@$1.min.json")"
+        fi
 fi
 echo "${output}"
